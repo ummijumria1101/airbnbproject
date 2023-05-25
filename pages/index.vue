8d1e4bed52aa97f1a1ad8f6694a16e4e226bad09
Ummi Jumria <58371327+ummijumria1101@users.noreply.github.com>
2023-05-25 19:39:03 +0700

    update homepage

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
                 <div class="pl-[25.65px] pt-[15.61px]">
                     <img src="~/assets/img/icons8-airbnb-50.png" class="w-[28.65px] h-[30.78px]">
                 </div>
-                <div class="flex gap-3 text-white absolute top-7 right-6">
+                <div class="flex gap-3 text-white absolute top-[27px] right-[24px]">
                     <NuxtLink to="/about" class="gap-3">satu</NuxtLink>
                     <NuxtLink to="https://twitter.com" class="gap-3">dua</NuxtLink>
                     <NuxtLink to="/about" class="gap-3">loreem</NuxtLink>
@@ -16,16 +16,39 @@
                     <NuxtLink to="/about" class="gap-3">tujuh</NuxtLink>
                 </div>
 
-                <div class="bg-white w-96 h-96 mt-20 rounded-sm">
+                <div class="bg-white w-[391px] h-[508px] mt-[132px] ml-[80px] rounded-sm">
+                    <div class="px-[24px] pt-[32px] text-2xl">
+                        <p>Pesanlah akomodasi dan nikmati aktivitas yang tak terlupakan!</p>
+                    </div>
 
                 </div>
 
-            </div>          
-      
-
-
+            </div>       
         </div>
-
+    </div>
+    <div class="">
+        <p class="mt-[88px] ml-[80px] mb-[24px] text-xl font-semibold">Explore Airbnb</p>
+        <div class="flex">
+            <div class="flex ml-[80px] w-[391px] h-[85px] rounded-md">
+                <img src="~/assets/img/bnbexplor.png" class="w-[121px] h-[85px]">
+                <p class="text-gray-400 text-lg text-center py-[33px] pl-[24px]">Perumahan</p>
+            </div>
+            <div class="flex ml-[80px] w-[391px] h-[85px] rounded-md">
+                <img src="~/assets/img/bnbexplor.png" class="w-[121px] h-[85px]">
+                <p class="text-gray-400 text-lg text-center py-[33px] pl-[24px]">Perumahan</p>
+            </div>
+            <div class="flex ml-[80px] w-[391px] h-[85px] rounded-md">
+                <img src="~/assets/img/bnbexplor.png" class="w-[121px] h-[85px]">
+                <p class="text-gray-400 text-lg text-center py-[33px] pl-[24px]">Perumahan</p>
+            </div>
+        </div>
+        <p class="mt-[88px] ml-[80px] text-xl font-semibold">Akomodasi Airbnb Plus</p>
+        <p class="mt-[8px] ml-[80px] text-base text-gray-400">Pilihan akomodasi diperiksa sesuai dengan kriteria kualitas dan desain</p>
+        <div>
+            <div class="bg-[url('~/assets/img/akomodasi.png')] bg-fixed">
+                
+            </div>
+        </div>
     </div>
   </template>
 
