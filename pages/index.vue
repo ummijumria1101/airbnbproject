1044f121c6a0e56310bdedcad1605995f6c0e8e5
Ummi Jumria <58371327+ummijumria1101@users.noreply.github.com>
2023-05-26 11:47:13 +0700

    update homepage

--- pages/index.vue
+++ pages/index.vue
@@ -64,10 +64,10 @@
                 <p class="text-sm text-gray-500">Cayman Islands</p>
                 <p class="text-base font-semibold">2 Nights PACKAGE All <br> Inclusive</p>
                 <p class="text-sm text-gray-500">Rp 10.000.000</p>
-                <!-- <div class="flex">
+                <div class="flex">
                     <p class="text-cyan-600">5.0 </p>
-                    <img src="img/icons8-star-50.png" class="w-[15px] h-[15px] items-center pl-1 mt-[5px]">
-                </div> -->
+                    <img src="/img/icons8-star-50.png" class="w-[15px] h-[15px] items-center pl-1 mt-[5px]">
+                </div>
                 
             </div>
             <div class="flex-none">
@@ -75,10 +75,10 @@
                 <p class="text-sm text-gray-500">Cayman Islands</p>
                 <p class="text-base font-semibold">2 Nights PACKAGE All <br> Inclusive</p>
                 <p class="text-sm text-gray-500">Rp 10.000.000</p>
-                <!-- <div class="flex">
+                <div class="flex">
                     <p class="text-cyan-600">5.0 </p>
-                    <img src="img/icons8-star-50.png" class="w-[15px] h-[15px] items-center pl-1 mt-[5px]">
-                </div> -->
+                    <img src="/img/icons8-star-50.png" class="w-[15px] h-[15px] items-center pl-1 mt-[5px]">
+                </div>
                 
             </div>
             <div class="flex-none">
@@ -86,10 +86,10 @@
                 <p class="text-sm text-gray-500">Cayman Islands</p>
                 <p class="text-base font-semibold">2 Nights PACKAGE All <br> Inclusive</p>
                 <p class="text-sm text-gray-500">Rp 10.000.000</p>
-                <!-- <div class="flex">
+                <div class="flex">
                     <p class="text-cyan-600">5.0 </p>
-                    <img src="img/icons8-star-50.png" class="w-[15px] h-[15px] items-center pl-1 mt-[5px]">
-                </div> -->
+                    <img src="/img/icons8-star-50.png" class="w-[15px] h-[15px] items-center pl-1 mt-[5px]">
+                </div>
                 
             </div>
             <div class="flex-none">
@@ -97,10 +97,10 @@
                 <p class="text-sm text-gray-500">Cayman Islands</p>
                 <p class="text-base font-semibold">2 Nights PACKAGE All <br> Inclusive</p>
                 <p class="text-sm text-gray-500">Rp 10.000.000</p>
-                <!-- <div class="flex">
+                <div class="flex">
                     <p class="text-cyan-600">5.0 </p>
-                    <img src="img/icons8-star-50.png" class="w-[15px] h-[15px] items-center pl-1 mt-[5px]">
-                </div> -->
+                    <img src="/img/icons8-star-50.png" class="w-[15px] h-[15px] items-center pl-1 mt-[5px]">
+                </div>
                 
             </div>
             <div class="flex-none">
@@ -108,10 +108,10 @@
                 <p class="text-sm text-gray-500">Cayman Islands</p>
                 <p class="text-base font-semibold">2 Nights PACKAGE All <br> Inclusive</p>
                 <p class="text-sm text-gray-500">Rp 10.000.000</p>
-                <!-- <div class="flex">
+                <div class="flex">
                     <p class="text-cyan-600">5.0 </p>
-                    <img src="img/icons8-star-50.png" class="w-[15px] h-[15px] items-center pl-1 mt-[5px]">
-                </div> -->
+                    <img src="/img/icons8-star-50.png" class="w-[15px] h-[15px] items-center pl-1 mt-[5px]">
+                </div>
                 
             </div>
             <div class="flex-none">
@@ -119,12 +119,227 @@
                 <p class="text-sm text-gray-500">Cayman Islands</p>
                 <p class="text-base font-semibold">2 Nights PACKAGE All <br> Inclusive</p>
                 <p class="text-sm text-gray-500">Rp 10.000.000</p>
-                <!-- <div class="flex">
+                <div class="flex">
                     <p class="text-cyan-600">5.0 </p>
-                    <img src="img/icons8-star-50.png" class="w-[15px] h-[15px] items-center pl-1 mt-[5px]">
-                </div> -->
+                    <img src="/img/icons8-star-50.png" class="w-[15px] h-[15px] items-center pl-1 mt-[5px]">
+                </div>
                 
             </div>
+
+        </div>
+
+        <div>
+            <p class="mt-[88px] ml-[80px] text-xl font-semibold">Logements dans le monde entier</p>
+             <p class="mt-[8px] ml-[80px] text-base text-gray-400 mb-[24px]">Voyages de plusieurs jours organisés par des experts locaux avec activités, repas et logements compris</p>
+        </div>
+
+        <div class="flex ml-[80px] gap-[8px]">
+            <div class="flex-none">
+                <img src="/img/superhost.png" class="w-[290px] h-[200px] rounded-md">
+                <div class="flex">
+                    <p class="font-semibold mt-[8px] border-[1px] border-slate-800 max-w-sm text-base py-2 px-3 rounded-sm text-slate-800">SUPERHOST</p>
+                    <p class="text-base text-slate-800 items-center pl-[13px] mt-[20px] w-[120px]">Toledo</p>
+                    <div class="flex">
+                        <img src="/img/icons8-star-50.png" class="w-[10px] h-[10px] items-center pl-1 mt-[26px] ml-[0px]">
+                    <p class="text-xs text-cyan-600 text-right mt-[22px] ml-[4px]">4,98</p>
+                    </div>
+                    
+                </div>
+                <p class="text-sm text-gray-500 mt-[8px]">À partir de 577€/personne - 3 jours</p>               
+         </div>
+         <div class="flex-none">
+                <img src="/img/superhost.png" class="w-[290px] h-[200px] rounded-md">
+                <div class="flex">
+                    <p class="font-semibold mt-[8px] border-[1px] border-slate-800 max-w-sm text-base py-2 px-3 rounded-sm text-slate-800">SUPERHOST</p>
+                    <p class="text-base text-slate-800 items-center pl-[13px] mt-[20px] w-[120px]">Toledo</p>
+                    <div class="flex">
+                        <img src="/img/icons8-star-50.png" class="w-[10px] h-[10px] items-center pl-1 mt-[26px] ml-[0px]">
+                    <p class="text-xs text-cyan-600 text-right mt-[22px] ml-[4px]">4,98</p>
+                    </div>
+                    
+                </div>
+                <p class="text-sm text-gray-500 mt-[8px]">À partir de 577€/personne - 3 jours</p>               
+         </div>
+         <div class="flex-none">
+                <img src="/img/superhost.png" class="w-[290px] h-[200px] rounded-md">
+                <div class="flex">
+                    <p class="font-semibold mt-[8px] border-[1px] border-slate-800 max-w-sm text-base py-2 px-3 rounded-sm text-slate-800">SUPERHOST</p>
+                    <p class="text-base text-slate-800 items-center pl-[13px] mt-[20px] w-[120px]">Toledo</p>
+                    <div class="flex">
+                        <img src="/img/icons8-star-50.png" class="w-[10px] h-[10px] items-center pl-1 mt-[26px] ml-[0px]">
+                    <p class="text-xs text-cyan-600 text-right mt-[22px] ml-[4px]">4,98</p>
+                    </div>
+                    
+                </div>
+                <p class="text-sm text-gray-500 mt-[8px]">À partir de 577€/personne - 3 jours</p>               
+         </div>
+         <div class="flex-none">
+                <img src="/img/superhost.png" class="w-[290px] h-[200px] rounded-md">
+                <div class="flex">
+                    <p class="font-semibold mt-[8px] border-[1px] border-slate-800 max-w-sm text-base py-2 px-3 rounded-sm text-slate-800">SUPERHOST</p>
+                    <p class="text-base text-slate-800 items-center pl-[13px] mt-[20px] w-[120px]">Toledo</p>
+                    <div class="flex">
+                        <img src="/img/icons8-star-50.png" class="w-[10px] h-[10px] items-center pl-1 mt-[26px] ml-[0px]">
+                    <p class="text-xs text-cyan-600 text-right mt-[22px] ml-[4px]">4,98</p>
+                    </div>
+                    
+                </div>
+                <p class="text-sm text-gray-500 mt-[8px]">À partir de 577€/personne - 3 jours</p>               
+         </div>
+        </div>
+        <div class="flex ml-[80px] gap-[8px] mt-[20px] mb-[88px]">
+            <div class="flex-none">
+                <img src="/img/superhost.png" class="w-[290px] h-[200px] rounded-md">
+                <div class="flex">
+                    <p class="font-semibold mt-[8px] border-[1px] border-slate-800 max-w-sm text-base py-2 px-3 rounded-sm text-slate-800">SUPERHOST</p>
+                    <p class="text-base text-slate-800 items-center pl-[13px] mt-[20px] w-[120px]">Toledo</p>
+                    <div class="flex">
+                        <img src="/img/icons8-star-50.png" class="w-[10px] h-[10px] items-center pl-1 mt-[26px] ml-[0px]">
+                    <p class="text-xs text-cyan-600 text-right mt-[22px] ml-[4px]">4,98</p>
+                    </div>
+                    
+                </div>
+                <p class="text-sm text-gray-500 mt-[8px]">À partir de 577€/personne - 3 jours</p>               
+         </div>
+         <div class="flex-none">
+                <img src="/img/superhost.png" class="w-[290px] h-[200px] rounded-md">
+                <div class="flex">
+                    <p class="font-semibold mt-[8px] border-[1px] border-slate-800 max-w-sm text-base py-2 px-3 rounded-sm text-slate-800">SUPERHOST</p>
+                    <p class="text-base text-slate-800 items-center pl-[13px] mt-[20px] w-[120px]">Toledo</p>
+                    <div class="flex">
+                        <img src="/img/icons8-star-50.png" class="w-[10px] h-[10px] items-center pl-1 mt-[26px] ml-[0px]">
+                    <p class="text-xs text-cyan-600 text-right mt-[22px] ml-[4px]">4,98</p>
+                    </div>
+                    
+                </div>
+                <p class="text-sm text-gray-500 mt-[8px]">À partir de 577€/personne - 3 jours</p>               
+         </div>
+         <div class="flex-none">
+                <img src="/img/superhost.png" class="w-[290px] h-[200px] rounded-md">
+                <div class="flex">
+                    <p class="font-semibold mt-[8px] border-[1px] border-slate-800 max-w-sm text-base py-2 px-3 rounded-sm text-slate-800">SUPERHOST</p>
+                    <p class="text-base text-slate-800 items-center pl-[13px] mt-[20px] w-[120px]">Toledo</p>
+                    <div class="flex">
+                        <img src="/img/icons8-star-50.png" class="w-[10px] h-[10px] items-center pl-1 mt-[26px] ml-[0px]">
+                    <p class="text-xs text-cyan-600 text-right mt-[22px] ml-[4px]">4,98</p>
+                    </div>
+                    
+                </div>
+                <p class="text-sm text-gray-500 mt-[8px]">À partir de 577€/personne - 3 jours</p>               
+         </div>
+         <div class="flex-none">
+                <img src="/img/superhost.png" class="w-[290px] h-[200px] rounded-md">
+                <div class="flex">
+                    <p class="font-semibold mt-[8px] border-[1px] border-slate-800 max-w-sm text-base py-2 px-3 rounded-sm text-slate-800">SUPERHOST</p>
+                    <p class="text-base text-slate-800 items-center pl-[13px] mt-[20px] w-[120px]">Toledo</p>
+                    <div class="flex">
+                        <img src="/img/icons8-star-50.png" class="w-[10px] h-[10px] items-center pl-1 mt-[26px] ml-[0px]">
+                    <p class="text-xs text-cyan-600 text-right mt-[22px] ml-[4px]">4,98</p>
+                    </div>
+                    
+                </div>
+                <p class="text-sm text-gray-500 mt-[8px]">À partir de 577€/personne - 3 jours</p>               
+         </div>
+        </div>
+
+        <div>
+            <p class="mt-[88px] ml-[80px] text-xl font-semibold">Temukan Petualangan Airbnb</p>
+             <p class="mt-[8px] ml-[80px] text-base text-gray-400 mb-[24px]">Nikmati perjalanan setiap hari dengan santai bersama ahli lokal. Kegiatan, makan, dan akomodasi sudah termasuk. Bergabunglah sekarang!</p>
+        </div>
+        <div class="flex ml-[80px] gap-[8px]">
+            <div class="flex-none">
+                <img src="/img/petualangan.png" class="w-[188px] h-[240px] rounded-md">
+                <p class="text-sm text-gray-500">Cayman Islands</p>
+                <p class="text-base font-semibold">2 Nights PACKAGE All <br> Inclusive</p>
+                <p class="text-sm text-gray-500">Rp 10.000.000</p>
+                <div class="flex">
+                    <p class="text-cyan-600">5.0 </p>
+                    <img src="/img/icons8-star-50.png" class="w-[15px] h-[15px] items-center pl-1 mt-[5px]">
+                </div>
+                
+            </div>
+            <div class="flex-none">
+                <img src="/img/petualangan.png" class="w-[188px] h-[240px] rounded-md">
+                <p class="text-sm text-gray-500">Cayman Islands</p>
+                <p class="text-base font-semibold">2 Nights PACKAGE All <br> Inclusive</p>
+                <p class="text-sm text-gray-500">Rp 10.000.000</p>
+                <div class="flex">
+                    <p class="text-cyan-600">5.0 </p>
+                    <img src="/img/icons8-star-50.png" class="w-[15px] h-[15px] items-center pl-1 mt-[5px]">
+                </div>
+                
+            </div>
+            <div class="flex-none">
+                <img src="/img/petualangan.png" class="w-[188px] h-[240px] rounded-md">
+                <p class="text-sm text-gray-500">Cayman Islands</p>
+                <p class="text-base font-semibold">2 Nights PACKAGE All <br> Inclusive</p>
+                <p class="text-sm text-gray-500">Rp 10.000.000</p>
+                <div class="flex">
+                    <p class="text-cyan-600">5.0 </p>
+                    <img src="/img/icons8-star-50.png" class="w-[15px] h-[15px] items-center pl-1 mt-[5px]">
+                </div>
+                
+            </div>
+            <div class="flex-none">
+                <img src="/img/petualangan.png" class="w-[188px] h-[240px] rounded-md">
+                <p class="text-sm text-gray-500">Cayman Islands</p>
+                <p class="text-base font-semibold">2 Nights PACKAGE All <br> Inclusive</p>
+                <p class="text-sm text-gray-500">Rp 10.000.000</p>
+                <div class="flex">
+                    <p class="text-cyan-600">5.0 </p>
+                    <img src="/img/icons8-star-50.png" class="w-[15px] h-[15px] items-center pl-1 mt-[5px]">
+                </div>
+                
+            </div>
+            <div class="flex-none">
+                <img src="/img/petualangan.png" class="w-[188px] h-[240px] rounded-md">
+                <p class="text-sm text-gray-500">Cayman Islands</p>
+                <p class="text-base font-semibold">2 Nights PACKAGE All <br> Inclusive</p>
+                <p class="text-sm text-gray-500">Rp 10.000.000</p>
+                <div class="flex">
+                    <p class="text-cyan-600">5.0 </p>
+                    <img src="/img/icons8-star-50.png" class="w-[15px] h-[15px] items-center pl-1 mt-[5px]">
+                </div>
+                
+            </div>
+            <div class="flex-none">
+                <img src="/img/petualangan.png" class="w-[188px] h-[240px] rounded-md">
+                <p class="text-sm text-gray-500">Cayman Islands</p>
+                <p class="text-base font-semibold">2 Nights PACKAGE All <br> Inclusive</p>
+                <p class="text-sm text-gray-500">Rp 10.000.000</p>
+                <div class="flex">
+                    <p class="text-cyan-600">5.0 </p>
+                    <img src="/img/icons8-star-50.png" class="w-[15px] h-[15px] items-center pl-1 mt-[5px]">
+                </div>
+                
+            </div>
+
+        </div>
+        
+
+        <div class="mt-[91px]">
+            <div>
+            <p class="mt-[88px] ml-[80px] text-xl font-semibold">Destinations Airbnb Plus à la Une</p>
+             <p class="mt-[8px] ml-[80px] text-base text-gray-400 mb-[24px]">Voyages de plusieurs jours organisés par des experts locaux avec activités, repas et logements compris</p>
+        </div>
+
+        <div class="flex ml-[80px] gap-[8px] mb-[313px]">
+            <div class="flex-none">
+                <img src="/img/destination.png" class="w-[391px] h-[239px] rounded-xl">
+                <p class="text-fuchsia-900 text-base font-semibold mt-[10px]">Plus de 200 séjours vérifiés</p>
+                <p class="text-sm text-gray-500 mt-[10px]">À partir de 577€/personne - 3 jours</p>               
+            </div>
+            <div class="flex-none">
+                <img src="/img/destination.png" class="w-[391px] h-[239px] rounded-xl">
+                <p class="text-fuchsia-900 text-base font-semibold mt-[10px]">Plus de 200 séjours vérifiés</p>
+                <p class="text-sm text-gray-500 mt-[10px]">À partir de 577€/personne - 3 jours</p>               
+            </div>
+            <div class="flex-none">
+                <img src="/img/destination.png" class="w-[391px] h-[239px] rounded-xl">
+                <p class="text-fuchsia-900 text-base font-semibold mt-[10px]">Plus de 200 séjours vérifiés</p>
+                <p class="text-sm text-gray-500 mt-[10px]">À partir de 577€/personne - 3 jours</p>               
+            </div>
+         </div>
 
         </div>
     </div>
