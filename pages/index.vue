e866588f3de214b71bbe7dfadcfef8e157591724
Ummi Jumria <58371327+ummijumria1101@users.noreply.github.com>
2023-05-26 07:11:30 +0700

    update home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,10 @@
 <template>
     <!-- <div style="background-image: url('https://i.postimg.cc/nctt4kKV/bg.png')" class="h-full"> -->
-    <div class="bg-[url('public/assets/img/bg.png')] bg-fixed bg-cover h-screen">
+    <div class="bg-[url('/img/bg.png')] bg-fixed bg-cover h-screen">
         <div >
             <div class="flex">
                 <div class="pl-[25.65px] pt-[15.61px]">
-                    <img src="~/assets/img/icons8-airbnb-50.png" class="w-[28.65px] h-[30.78px]">
+                    <img src="/img/icons8-airbnb-50.png" class="w-[28.65px] h-[30.78px]">
                 </div>
                 <div class="flex gap-3 text-white absolute top-[27px] right-[24px]">
                     <NuxtLink to="/about" class="gap-3">satu</NuxtLink>
@@ -30,27 +30,107 @@
         <p class="mt-[88px] ml-[80px] mb-[24px] text-xl font-semibold">Explore Airbnb</p>
         <div class="flex">
             <div class="flex ml-[80px] w-[391px] h-[85px] rounded-md">
-                <img src="~/assets/img/bnbexplor.png" class="w-[121px] h-[85px]">
+                <img src="/img/bnbexplor.png" class="w-[121px] h-[85px]">
                 <p class="text-gray-400 text-lg text-center py-[33px] pl-[24px]">Perumahan</p>
             </div>
             <div class="flex ml-[80px] w-[391px] h-[85px] rounded-md">
-                <img src="~/assets/img/bnbexplor.png" class="w-[121px] h-[85px]">
+                <img src="/img/bnbexplor.png" class="w-[121px] h-[85px]">
                 <p class="text-gray-400 text-lg text-center py-[33px] pl-[24px]">Perumahan</p>
             </div>
             <div class="flex ml-[80px] w-[391px] h-[85px] rounded-md">
-                <img src="~/assets/img/bnbexplor.png" class="w-[121px] h-[85px]">
+                <img src="/img/bnbexplor.png" class="w-[121px] h-[85px]">
                 <p class="text-gray-400 text-lg text-center py-[33px] pl-[24px]">Perumahan</p>
             </div>
         </div>
         <p class="mt-[88px] ml-[80px] text-xl font-semibold">Akomodasi Airbnb Plus</p>
         <p class="mt-[8px] ml-[80px] text-base text-gray-400">Pilihan akomodasi diperiksa sesuai dengan kriteria kualitas dan desain</p>
         <div>
-            <div class="bg-[url('~/assets/img/akomodasi.png')] bg-fixed">
+            <div class="bg-[url('/img/akomodasi.png')] bg-cover bg-center h-[310px] mx-[80px] rounded-md">
+                <div class="item-center">
+                    <img src="/img/logobw.png" class="ml-[550.66px] mt-[10px] pt-[10px] w-[104.5px] h-[112.7px]">
+                    <p class="ml-[500px] mr-[500px] mt-[232] mb-[66px] w-[200px] h-[44px] align-middle text-center bg-white">Temukan akomodasi</p>
+                </div>
                 
             </div>
+        </div>
+
+        <div>
+            <p class="mt-[88px] ml-[80px] text-xl font-semibold">Temukan Petualangan Airbnb</p>
+             <p class="mt-[8px] ml-[80px] text-base text-gray-400 mb-[24px]">Nikmati perjalanan setiap hari dengan santai bersama ahli lokal. Kegiatan, makan, dan akomodasi sudah termasuk. Bergabunglah sekarang!</p>
+        </div>
+        <div class="flex ml-[80px] gap-[8px]">
+            <div class="flex-none">
+                <img src="/img/petualangan.png" class="w-[188px] h-[240px] rounded-md">
+                <p class="text-sm text-gray-500">Cayman Islands</p>
+                <p class="text-base font-semibold">2 Nights PACKAGE All <br> Inclusive</p>
+                <p class="text-sm text-gray-500">Rp 10.000.000</p>
+                <div class="flex">
+                    <p class="text-cyan-600">5.0 </p>
+                    <img src="img/icons8-star-50.png" class="w-[15px] h-[15px] items-center pl-1 mt-[5px]">
+                </div>
+                
+            </div>
+            <div class="flex-none">
+                <img src="/img/petualangan.png" class="w-[188px] h-[240px] rounded-md">
+                <p class="text-sm text-gray-500">Cayman Islands</p>
+                <p class="text-base font-semibold">2 Nights PACKAGE All <br> Inclusive</p>
+                <p class="text-sm text-gray-500">Rp 10.000.000</p>
+                <div class="flex">
+                    <p class="text-cyan-600">5.0 </p>
+                    <img src="img/icons8-star-50.png" class="w-[15px] h-[15px] items-center pl-1 mt-[5px]">
+                </div>
+                
+            </div>
+            <div class="flex-none">
+                <img src="/img/petualangan.png" class="w-[188px] h-[240px] rounded-md">
+                <p class="text-sm text-gray-500">Cayman Islands</p>
+                <p class="text-base font-semibold">2 Nights PACKAGE All <br> Inclusive</p>
+                <p class="text-sm text-gray-500">Rp 10.000.000</p>
+                <div class="flex">
+                    <p class="text-cyan-600">5.0 </p>
+                    <img src="img/icons8-star-50.png" class="w-[15px] h-[15px] items-center pl-1 mt-[5px]">
+                </div>
+                
+            </div>
+            <div class="flex-none">
+                <img src="/img/petualangan.png" class="w-[188px] h-[240px] rounded-md">
+                <p class="text-sm text-gray-500">Cayman Islands</p>
+                <p class="text-base font-semibold">2 Nights PACKAGE All <br> Inclusive</p>
+                <p class="text-sm text-gray-500">Rp 10.000.000</p>
+                <div class="flex">
+                    <p class="text-cyan-600">5.0 </p>
+                    <img src="img/icons8-star-50.png" class="w-[15px] h-[15px] items-center pl-1 mt-[5px]">
+                </div>
+                
+            </div>
+            <div class="flex-none">
+                <img src="/img/petualangan.png" class="w-[188px] h-[240px] rounded-md">
+                <p class="text-sm text-gray-500">Cayman Islands</p>
+                <p class="text-base font-semibold">2 Nights PACKAGE All <br> Inclusive</p>
+                <p class="text-sm text-gray-500">Rp 10.000.000</p>
+                <div class="flex">
+                    <p class="text-cyan-600">5.0 </p>
+                    <img src="img/icons8-star-50.png" class="w-[15px] h-[15px] items-center pl-1 mt-[5px]">
+                </div>
+                
+            </div>
+            <div class="flex-none">
+                <img src="/img/petualangan.png" class="w-[188px] h-[240px] rounded-md">
+                <p class="text-sm text-gray-500">Cayman Islands</p>
+                <p class="text-base font-semibold">2 Nights PACKAGE All <br> Inclusive</p>
+                <p class="text-sm text-gray-500">Rp 10.000.000</p>
+                <div class="flex">
+                    <p class="text-cyan-600">5.0 </p>
+                    <img src="img/icons8-star-50.png" class="w-[15px] h-[15px] items-center pl-1 mt-[5px]">
+                </div>
+                
+            </div>
+
         </div>
     </div>
   </template>
 
-  <!-- url('~/assets/img/bg.png') -->
+  <!-- url('~/assets/img/bg.png')
+
+pr-[630.84] pt-[65.87px] pb-[131.87] -->
   
