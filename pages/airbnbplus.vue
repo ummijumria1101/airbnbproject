64100193ea7b544908d9f5fb161f6461b58cd7d0
Ummi Jumria <58371327+ummijumria1101@users.noreply.github.com>
2023-05-28 15:47:58 +0700

    update

---
+++ pages/airbnbplus.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="flex border-2 h-[81px] items-center justify-between">
+        <img src="/img/airbnbpinklogo.png" class="h-[81px] w-auto items-center">
+        <div class="w-[352px] h-[48px] rounded-3xl border-2">
+
+        </div>
+        <div class="flex gap-2 items-center pr-[10px]">
+            <p class="font-bold">Become a Host</p>
+            <img src="/img/globe.png" class="h-[16px] w[16px]">
+            <div class="w-[81px] h-[38px] rounded-3xl border-2">
+
+            </div>
+        </div>
+    </div>
+    <div>
+        <petualangan />
+        <temukan />
+        <villa />
+        <destinasi />
+        
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="" scoped>
+
+</style>
