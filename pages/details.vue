22e9cec49ac38a6b268111c7d1b74701389dc366
Ummi Jumria <58371327+ummijumria1101@users.noreply.github.com>
2023-05-31 23:39:30 +0700

    Update details.vue

--- pages/details.vue
+++ pages/details.vue
@@ -1,6 +1,8 @@
 <template>
-    <div class="flex border-2 h-[81px] items-center justify-between">
-        <img src="/img/airbnbpinklogo.png" class="h-[81px] w-auto items-center">
+    <div class="flex border-b-2 h-[81px] items-center justify-between">
+        <NuxtLink to="/">
+            <img src="/img/airbnbpinklogo.png" class="h-[81px] w-auto items-center">
+        </NuxtLink>
         <div class="w-[352px] h-[48px] rounded-3xl border-2">
 
         </div>
@@ -14,48 +16,211 @@
     </div>
     <div>
         <div class="bg-white">
-        <h1 class="mb-10 font-bold text-2xl text-primarycolor tracking-wide ml-[80px] mt-[20px]">VILLA Pavilion Garden dengan KOLAM RENANG PRIBADI</h1>
+        <h1 class="font-medium text-3xl text-[#222222] tracking-wide ml-[80px] mt-[20px]">VILLA Pavilion Garden dengan KOLAM RENANG PRIBADI</h1>
+        <div class="flex mb-[20px] items-center gap-[10px] ml-[80px]">
+                                    <div class="flex gap-[3px] items-center">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-3 h-3">
+                                            <path fill-rule="evenodd" d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z" clip-rule="evenodd" />
+                                        </svg>
+                                    <p class="text-sm font-medium">4.92</p>
+                                    <p class="text-gray-600 text-sm">(96 ulasan)</p>
+                                    </div>
+                                    
+
+                                    <div class="flex gap-[3px] items-center mr-[10px]">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-3 h-3">
+                                            <path fill-rule="evenodd" d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z" clip-rule="evenodd" />
+                                        </svg>
+                                        <p class="text-gray-600 text-sm">HostTeladan</p>
+                                    </div>  
+                                    <div>
+                                        <p class="font-semibold underline underline-offset-1">Bandung, Jawa Barat, Indonesia</p>
+                                    </div>                         
+                                </div>
         
-    </div>
-    <div class="mb-[10px] flex gap-[5px] ">
-        <img src="/img/superhost.png" class="w-40% ml-[80px] rounded-l-xl">
-        <div class="grid grid-cols-2 gap-[5px] rounded-r-xl">
-            <img src="/img/superhost.png" class="w-[290px] h-[200px] ">
-            <img src="/img/superhost.png" class="w-[290px] h-[200px] ">
-            <img src="/img/superhost.png" class="w-[290px] h-[200px] ">
-            <img src="/img/superhost.png" class="w-[290px] h-[200px] ">
-
         </div>
-    </div >
-    <div class="flex mt-[100-px] justify-between mr-[100px]">
-        <div class="border-b ml-[80px]">
-            <p class="font-base text-2xl">Seluruh vila. Tuan rumah: Diana</p>
-            <p class="pb-[10px]">10 tamu  3 kamar tidur  9 kamar tidur  2 kamar mandi + ruang toilet</p>
-        </div>
-        <div class="border-2 p-2 ml-4 w-[373px] h-[532px] rounded-lg justify-center">
-            <div class="flex">
-                <p class="font-bold text-lg m-[24px]">Rp2.248.000 malam</p>
-                <p></p>
-                <p class="text-sm text-gray-600 items-center right-0 m-[30px]">96 ulasan</p>
+        <!-- gambar pembuka -->
+        <div class="mb-[10px] flex gap-[5px] ">
+            <img src="/img/superhost.png" class="w-40% ml-[80px] rounded-l-xl">
+            <div class="grid grid-cols-2 gap-[5px] rounded-r-xl">
+                <img src="/img/superhost.png" class="w-[290px] h-[200px] ">
+                <img src="/img/superhost.png" class="w-[290px] h-[200px] rounded-tr-xl ">
+                <img src="/img/superhost.png" class="w-[290px] h-[200px] ">
+                <img src="/img/superhost.png" class="w-[290px] h-[200px] rounded-br-xl">
             </div>
-            <div class="grid grid-cols-2 ml-[24px] text-xs">
-                <p class="h-[56px] border-x-2 border-t-2 rounded-tl-md">CHECK-IN</p>
-                <p class="h-[56px] border-t-2 border-r-2 rounded-tr-md ">CHECK-IN</p>
-                <p class="h-[56px] border-2 col-span-2 rounded-b-md">CHECK</p>
+        </div >
 
+        <div class="flex mt-[100-px] justify-between mr-[100px]">
+            <div class="ml-[80px] w-[700px]">
+                <div class="flex justify-between items-center">
+                    <div class="flex-none">
+                    <p class="font-base text-2xl">Seluruh vila. Tuan rumah: Diana</p>
+                    <p class="pb-[10px]">10 tamu  3 kamar tidur  9 kamar tidur  2 kamar mandi + ruang toilet</p>
+                    </div>
+                    <div class="">
+                        <div class="bg-[url('/img/dest2.png')] bg-cover w-[40px] h-[40px] rounded-full">
+                            <!-- <div class="">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5 ml-[30px] top-[10px]">
+                                            <path fill-rule="evenodd" d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z" clip-rule="evenodd" />
+                                        </svg>
+                            </div> -->
+
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <!-- isi keterangan host teladan dll -->
+                    <div class="pb-[20px] border-b-2">
+                        
+                        <div class="flex gap-5 items-start mb-[20px]">
+                            <!-- pertama -->
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+                            </svg>
+                            <div>
+                                <p class="font-semibold">
+                                Diana adalah seorang HosTeladan.
+                            </p>
+                            <p class="text-gray-600 overflow-y-visible">HosTeladan adalah tuan rumah berpengalaman dan berpenilaian tinggi yang berkomitmen dalam menyediakan penginapan luar biasa bagi tamu.</p>
+                            </div>
+                        </div>
+                        <div class="flex gap-5 items-start mb-[20px]">
+                            <!-- kedua -->
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 5.25a3 3 0 013 3m3 0a6 6 0 01-7.029 5.912c-.563-.097-1.159.026-1.563.43L10.5 17.25H8.25v2.25H6v2.25H2.25v-2.818c0-.597.237-1.17.659-1.591l6.499-6.499c.404-.404.527-1 .43-1.563A6 6 0 1121.75 8.25z" />
+                            </svg>
+                            <div>
+                                <p class="font-semibold">Pengalaman check in istimewa
+                            </p>
+                            <p class="text-gray-600 overflow-y-visible">90% tamu baru-baru ini memberikan nilai 5 bintang untuk proses check-in.</p>
+                            </div>
+                        </div>
+                        <div class="flex gap-5 items-start mb-[20px]">
+                            <!-- ketiga -->
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z" />
+                            </svg>                            
+                            <div>
+                                <p class="font-semibold">
+                                    Pembatalan gratis sebelum 11 Jul.
+                            </p>
+                            <!-- <p class="text-gray-600 overflow-y-visible">Pembatalan gratis sebelum 11 Jul.</p> -->
+                            </div>
+                        </div>                       
+                    </div>
+
+                    <!-- LOKASI UTAMA -->
+                    <div class="pb-[20px] border-b-2">
+                            <p>LOKASI UTAMA</p>
+                            <ul class="list-disc ml-[20px]">
+                                <li>10 meter ke Toko Teh dan Kopi yang nyaman</li>
+                                <li>3,5 km ke McDonalds Simpang Dago</li>
+                                <li>4,8 km ke Ruang Seni Selasar Sunaryo</li>
+                                <li>2,4 km ke Wi - Fi Kuliner Tradisional Warung Lela.</li>
+                            </ul>
+                            <p>...</p>
+                            <p class="underline underline-offset-1 font-semibold">Tampilkan lebih banyak</p>
+                    </div>
+
+                    <!-- fasilitas yg ditawarkan -->
+                    <div>
+                        <p class="font-base text-2xl">Fasilitas yang ditawarkan</p>
+                    </div>
+                    <!-- isi fasilitas -->
+                    <div class="flex justify-evenly">
+                        <!-- kiri -->
+                        <div>
+                        <!-- dapur -->
+                        <div class="flex gap-3 m-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+                            </svg>
+                            <p>Dapur</p>
+                        </div>
+
+                        <!-- area kerja khusus -->
+                        <div class="flex gap-3 m-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 00.75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 00-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0112 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 01-.673-.38m0 0A2.18 2.18 0 013 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 013.413-.387m7.5 0V5.25A2.25 2.25 0 0013.5 3h-3a2.25 2.25 0 00-2.25 2.25v.894m7.5 0a48.667 48.667 0 00-7.5 0M12 12.75h.008v.008H12v-.008z" />
+                            </svg>
+                            <p>Area kerja khusus</p>
+                        </div>
+
+                        <!-- wifi -->
+                        <div class="flex gap-3 m-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M8.288 15.038a5.25 5.25 0 017.424 0M5.106 11.856c3.807-3.808 9.98-3.808 13.788 0M1.924 8.674c5.565-5.565 14.587-5.565 20.152 0M12.53 18.22l-.53.53-.53-.53a.75.75 0 011.06 0z" />
+                            </svg>
+                            <p>Wifi</p>
+                        </div>
+
+
+                    </div>
+
+                    <!-- kanan -->
+                    <div>
+                        <!-- dapur -->
+                        <div class="flex gap-3 m-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+                            </svg>
+                            <p>Dapur</p>
+                        </div>
+
+                        <!-- area kerja khusus -->
+                        <div class="flex gap-3 m-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 00.75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 00-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0112 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 01-.673-.38m0 0A2.18 2.18 0 013 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 013.413-.387m7.5 0V5.25A2.25 2.25 0 0013.5 3h-3a2.25 2.25 0 00-2.25 2.25v.894m7.5 0a48.667 48.667 0 00-7.5 0M12 12.75h.008v.008H12v-.008z" />
+                            </svg>
+                            <p>Area kerja khusus</p>
+                        </div>
+
+                        <!-- wifi -->
+                        <div class="flex gap-3 m-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M8.288 15.038a5.25 5.25 0 017.424 0M5.106 11.856c3.807-3.808 9.98-3.808 13.788 0M1.924 8.674c5.565-5.565 14.587-5.565 20.152 0M12.53 18.22l-.53.53-.53-.53a.75.75 0 011.06 0z" />
+                            </svg>
+                            <p>Wifi</p>
+                        </div>
+
+
+                    </div>
+                    </div>
+                    
+
+                </div>
+
+                    
+                </div>
+
+                <div class="border-2 p-2 ml-4 w-[373px] h-[532px] rounded-lg justify-center">
+                <div class="flex">
+                    <p class="font-bold text-lg m-[24px]">Rp2.248.000 malam</p>
+                    <p></p>
+                    <p class="text-sm text-gray-600 items-center right-0 m-[30px]">96 ulasan</p>
+                </div>
+                <div class="grid grid-cols-2 ml-[24px] text-xs">
+                    <p class="h-[56px] border-x-2 border-t-2 rounded-tl-md">CHECK-IN</p>
+                    <p class="h-[56px] border-t-2 border-r-2 rounded-tr-md ">CHECK-IN</p>
+                    <p class="h-[56px] border-2 col-span-2 rounded-b-md">CHECK</p>
+
+                </div>
+                <NuxtLink to="/checkoutpage">
+                    <div class="p-2 bg-pink-600 ml-[24px] mt-[10px] rounded-md">
+                    <p class="text-center font-bold text-white">Pesan</p>
+                </div>
+                </NuxtLink>
+                
             </div>
-            <NuxtLink to="/checkoutpage">
-                <div class="p-2 bg-pink-600 ml-[24px] mt-[10px] rounded-md">
-                <p class="text-center font-bold text-white">Pesan</p>
             </div>
-            </NuxtLink>
+
+
             
         </div>
-    </div>
+        <div>
+            <!-- ulasan dll -->
+        </div>
         
-        
-
-    </div>
 </template>
 
 <script setup>
